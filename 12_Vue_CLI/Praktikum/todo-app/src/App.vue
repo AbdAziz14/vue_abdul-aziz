2a0769c2b94d405c6e76a65bfcd73cb76fdcc580
AbdAziz4212 <abdaziz140401@gmail.com>
2022-03-16 19:09:46 +0800

    update: condition in method vue

--- 12_Vue_CLI/Praktikum/todo-app/src/App.vue
+++ 12_Vue_CLI/Praktikum/todo-app/src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <div class="todo-wrapper">
+        <ol>
+            <li v-for="todo in toDoList" :key="todo" >
+                {{ todo }}
+            </li>
+        </ol>
+
+        <input type="text" v-model="task">
+        <button @click="addTask">Tambahkan</button>
+
+        <p>{{ message }}</p>
+    </div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+            message: '',
+            toDoList: [],
+            task: '',
+        }
+    },
+    methods: {
+        addTask: function(){
+            
+            if(this.task){
+                if(this.toDoList.length < 4){
+                    this.toDoList.push(this.task);
+                    this.task = "";
+                }else{
+                    this.message = "Hebat!";
+                }
+            }else{
+                this.message = "Masih Kosong!";
+            }
+
+        }
+    },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
+
+.todo-wrapper {
+    width: 50%;
+    margin: auto;
+}
+
+.todo-wrapper ol {
+    list-style-position: inside;
+}
+
 </style>
