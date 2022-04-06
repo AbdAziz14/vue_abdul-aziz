ab0b80c7a8ea748216c81e45f8be498c90ab4f62
AbdAziz4212 <abdaziz140401@gmail.com>
2022-04-06 11:38:55 +0800

    add: summary.md in vue ui framework

--- news-app/src/views/HomeView.vue
+++ news-app/src/views/HomeView.vue
@@ -1,49 +1,14 @@
 <template>
-  <div class="home">
-    <h1 class="home-title">Berita Terkini</h1>
-    <p>{{ getinfo }}</p>
-    <div class="news-wrapper">
-      <NewsPage v-for="(value, index) in newsList" :key="index" :News="value" />
-    </div>
-  </div>
+  <hello-world />
 </template>
 
 <script>
-import NewsPage from '@/components/NewsView.vue'
+import HelloWorld from '../components/HelloWorld'
 
 export default {
   name: 'HomeView',
   components: {
-    NewsPage
-  },
-  computed: {
-    newsList () {
-      return this.$store.state.news.newsList
-    },
-    getinfo () {
-      return this.$store.state.news.info
-    }
-  },
-  mounted () {
-    this.$store.dispatch('news/getFetchList')
+    HelloWorld
   }
 }
 </script>
-
-<style scoped>
-* {
-  box-sizing: border-box;
-}
-.home {
-  margin-top: 5em;
-}
-.home-title {
-  font-size: 5em;
-}
-.news-wrapper {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  margin: 0;
-}
-</style>
